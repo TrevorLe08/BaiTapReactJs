a9f2ea7ddd4b603a923562a710b9b43dbba603f2
TrevorLe08 <letranminhtriet2307@gmail.com>
2024-05-08 20:16:57 +0700

    Add bt

--- BTphan1/bt5.js
+++ BTphan1/bt5.js
@@ -1,4 +1,4 @@
 // 5. Sử dụng reduce, tính tổng các phần tử * 2
 let arr = [1,2,3,4,5,6,7,8]
-let sum = arr.reduce((value,x) => value+x)
-console.log(sum*2);
+let sum = arr.reduce((value,x) => value+x*2,0)
+console.log(sum);
