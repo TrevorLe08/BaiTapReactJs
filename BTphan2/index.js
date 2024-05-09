31ebd1409ff87c3ca374a4caed28ec58a80220da
TrevorLe08 <letranminhtriet2307@gmail.com>
2024-05-09 18:40:00 +0700

    Add file

---
+++ BTphan2/index.js
@@ -0,0 +1,42 @@
+// 1. Viết 1 arrow function nhận vào 1 số, trả về true nếu số nguyên tố, false nếu không nguyên tố
+// 2. Viết 1 arrow function nhận vào 1 mảng, sử dụng map để in ra các số nguyên tố trong mảng
+// 3. Viết 1 arrow function nhận vào 1 mảng, trả về 1 mảng chỉ gồm các phần tử số nguyên tố, sử dụng filter
+// 4. Viết 1 arrow function nhận vào 1 mảng, trả về 1 tổng các bình phương từng phần tử, sử dụng reduce
+
+let a = 10
+let arr = [3, 4, 5, 6, 7, 8]
+// BT1
+let f1 = num => { // check prime
+    if (num < 2) return false
+    if (num == 2) return true
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return false
+        }
+    }
+    return true
+}
+
+
+console.log(f1(a));
+console.log("-------------------------------------");
+
+// BT2
+let f2 = array => {
+    array.map(x => {
+        if (f1(x) == true) {
+            console.log(x);
+        }
+    })
+}
+f2(arr)
+console.log("-------------------------------------");
+
+// Bt3
+let f3 = array => array.filter(x => f1(x))
+console.log(f3(arr))
+console.log("-------------------------------------");
+
+// Bt4 
+let f4 = array => array.reduce((value,x) => value + x*x,0)
+console.log(f4(arr));
---
+++ BTphan3/index.js
@@ -0,0 +1,59 @@
+// Tạo mảng sản phẩm có id, name, price, quantity.
+// 1. Sử dung map, in ra name và price cua cac sản phẩm
+// 2. Sử dụng map, tìm ra các  sản phẩm có giá > 100
+// 3. Sử dụng filter, tìm ra các  sản phẩm có giá < 100
+// 4. Sử dụng map, tính tổng tiền hàng sẽ có nếu bán hết toàn bộ sản phẩm (tổng giá*số lượng)
+// 5. Sử dụng reduce, tính tổng tiền hàng sẽ có nếu bán hết toàn bộ sản phẩm (tổng giá*số lượng)
+
+let arrProduct = [
+    { id: 1, name: "Coca Cola", price: 100, quantity: 5 },
+    { id: 2, name: "Pepsi", price: 90, quantity: 10 },
+    { id: 3, name: "Fanta", price: 75, quantity: 15 },
+    { id: 4, name: "Sprite", price: 110, quantity: 2 },
+    { id: 5, name: "Mirinda", price: 120, quantity: 3 }
+]
+
+console.log("---1. Sử dung map, in ra name và price cua cac sản phẩm---");
+// BT1
+
+let f1 = (array) => {
+    array.map(x => console.log("Tên sản phẩm:",x.name,".Giá thành:",x.price,"VNĐ"))
+}
+f1(arrProduct)
+console.log("---2. Sử dụng map, tìm ra các  sản phẩm có giá > 100---");
+
+// BT2
+let f2 = (array) => {
+    array.map(x => {
+        if (x.price > 100) {
+            console.log("Tên sản phẩm:",x.name,".Giá thành:",x.price,"VNĐ");
+        }
+    })
+}
+f2(arrProduct)
+console.log("---3. Sử dụng filter, tìm ra các  sản phẩm có giá < 100---");
+
+// BT3
+let f3 = (array) => {
+    let newArr = array.filter(x => x.price < 100)
+    newArr.map(x => console.log("Tên sản phẩm:",x.name,".Giá thành:",x.price,"VNĐ"))
+}
+f3(arrProduct)
+console.log("---4. Sử dụng map, tính tổng tiền hàng sẽ có nếu bán hết toàn bộ sản phẩm---");
+
+// BT4
+let f4 = (array) => {
+    let sum = 0
+    let newArr = array.map(x => x.price*x.quantity)
+    newArr.map(x => sum += x)
+    console.log("Giá tiền khi bán hết sản phẩm",sum);
+}
+f4(arrProduct)
+console.log("---5. Sử dụng reduce, tính tổng tiền hàng sẽ có nếu bán hết toàn bộ sản phẩm---");
+
+// BT5
+let f5 = (array) => {
+    let sum = array.map(x => x.price*x.quantity).reduce((acc,value) => acc + value, 0)
+    console.log("Giá tiền khi bán hết sản phẩm",sum);
+}
+f5(arrProduct)
